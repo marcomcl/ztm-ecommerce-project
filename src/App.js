3892ad898fbc36a1891479a37bd24f22a185bda9
marcomcl <ashba1996@gmail.com>
2023-12-01 08:50:24 +0100

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {Routes, Route} from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component';
-
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index={true} element={<Home />}/>
+      </Route>
     </Routes>
   );
 }
