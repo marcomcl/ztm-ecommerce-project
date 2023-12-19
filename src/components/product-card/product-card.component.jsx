8f2785eac5309151556d66dccacf489a17ca74a6
marcomcl <ashba1996@gmail.com>
2023-12-19 19:49:23 +0100

    Removed product-card.styles.scss and created ProductCardContainer styled component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -2,9 +2,16 @@ import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import {
+  ProductCardContainer,
+  ProductImg,
+  ProductButton,
+  ProductFooter,
+  ProductName,
+  ProductPrice,
+} from "./product-card.styles";
 
-import "./product-card.styles.scss";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -13,19 +20,19 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className="product-card-container">
-      <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <Button
+    <ProductCardContainer>
+      <ProductImg src={imageUrl} alt={`${name}`} />
+      <ProductFooter>
+        <ProductName>{name}</ProductName>
+        <ProductPrice>{price}</ProductPrice>
+      </ProductFooter>
+      <ProductButton
         buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to cart
-      </Button>
-    </div>
+      </ProductButton>
+    </ProductCardContainer>
   );
 };
 
