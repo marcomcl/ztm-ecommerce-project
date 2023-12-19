587eb3c85c8b1ecef420247f7feb258f1e4986a7
marcomcl <ashba1996@gmail.com>
2023-12-19 19:04:21 +0100

    Removed category.styles.scss and created CategoryContainer styled component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,8 +2,8 @@ import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import { CategoriesContext } from "../../contexts/categories.context";
+import { CategoryContainer, CategoryTitle } from "./category.styles";
 
-import "./category.styles.scss";
 import ProductCard from "../../components/product-card/product-card.component";
 
 const Category = () => {
@@ -17,13 +17,13 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 };
