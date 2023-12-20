3371da43ad58712a0d84843855ea49457df84e94
marcomcl <ashba1996@gmail.com>
2023-12-20 08:34:36 +0100

    Removed directory-item.styles.scss and created DirectoryItemContainer styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,20 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  CategoryBodyContainer,
+  CategoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
 
   return (
-    <div className="category-item-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="category-body-container">
+    <CategoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <CategoryBodyContainer>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </CategoryBodyContainer>
+    </CategoryItemContainer>
   );
 };
 
