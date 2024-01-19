951c5f9af48d7972aa4bcb49b63d720dc2c6fc6e
marcomcl <ashba1996@gmail.com>
2024-01-19 21:46:51 +0100

    Memoized categories selector

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -4,7 +4,10 @@ export const CATEGORIES_INITIAL_STATE = {
   categoriesArray: [],
 };
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action) => {
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {}
+) => {
   const { type, payload } = action;
   switch (type) {
     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
