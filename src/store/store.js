34f8652a91aa6147a991a05894e33353b648fe9b
marcomcl <ashba1996@gmail.com>
2024-01-19 21:06:45 +0100

    Custom logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,20 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
+
+const logger = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
 
 const middlewares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middlewares));
