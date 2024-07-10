740c5ea7e7b90ec492019985615cf529188dcd6d
marcomcl <ashba1996@gmail.com>
2024-07-10 11:48:12 +0200

    Typed root store

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -13,7 +13,7 @@ export function withMatcher(actionCreator: Function) {
     return Object.assign(actionCreator, {
         type,
         match(action: AnyAction) {
-            action.type === type;
+            return action.type === type;
         }
     })
 }
